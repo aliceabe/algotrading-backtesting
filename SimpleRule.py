5233ae670780dd5293d3162b812e1e1df7c6e43a
Akshaan Kakar <akshaan.crackers@gmail.com>
2015-12-15 16:17:20 -0500

    Added script to calculate portfolio metrics

--- SimpleRule.py
+++ SimpleRule.py
@@ -2,6 +2,7 @@
 from pyspark import SparkContext
 import os
 
+
 def simpleRule(dataPoint):
 	global canbuy
 	global nbshares
@@ -16,11 +17,11 @@ def simpleRule(dataPoint):
 		nbshares = 0
 		canbuy = True
 	current = capital + nbshares * dataPoint[1] - 1000
-	with open("/home/alice/Documents/algotrading-backtesting/quantquote_daily_sp500_83986/profits/" + code + ".csv", "a") as myfile:
+	with open("/Users/akshaankakar/Desktop/Big_Data_Analytics/Final/quantquote_daily_sp500_83986/profits/" + code + ".csv", "a") as myfile:
 		myfile.write(dataPoint[0] + ',' + str(current) + '\n')
 
 sc = SparkContext("local", "SimpleRule")
-for filename in os.listdir('/home/alice/Documents/algotrading-backtesting/quantquote_daily_sp500_83986/quotes'):
+for filename in os.listdir('/Users/akshaankakar/Desktop/Big_Data_Analytics/Final/quantquote_daily_sp500_83986/quotes'):
 
 	try:
 		code = filename.split('_')[1].split('.')[0]
@@ -28,7 +29,7 @@ for filename in os.listdir('/home/alice/Documents/algotrading-backtesting/quantq
 		nbshares = 0
 		capital = 1000
 
-		quoteFile = "/home/alice/Documents/algotrading-backtesting/quantquote_daily_sp500_83986/quotes/" + filename
+		quoteFile = "/Users/akshaankakar/Desktop/Big_Data_Analytics/Final/quantquote_daily_sp500_83986/quotes/" + filename
 		quoteData = sc.textFile(quoteFile).cache()
 
 		rdd = quoteData.map(lambda line: line.split(',')).map(lambda x: (x[0], (float(x[2])+float(x[5]))/2))
---
+++ calculateMetrics.py
@@ -0,0 +1,33 @@
+import os
+import csv
+import json
+import math
+
+for filename in os.listdir('/Users/akshaankakar/Desktop/Big_Data_Analytics/Final/quantquote_daily_sp500_83986/profits'):
+	returns = []
+	print filename
+	with open("/Users/akshaankakar/Desktop/Big_Data_Analytics/Final/quantquote_daily_sp500_83986/profits/" + filename, "r") as myfile:	
+		reader = csv.reader(myfile)
+		for row in reader:
+			returns.append(float(row[1]))		
+	mean = float(sum((returns)))/len(returns)
+	std = map(lambda x: (x - mean)**2,returns)
+	std = sum(std)/len(std)
+	max_val = max(returns)
+	min_val = min(returns)
+	max_drawdown = 0
+	if(max_val != 0):
+		max_drawdown = (max_val - min_val)/max_val
+	sharpe = [0 for x in range(0,len(returns))]
+	if(max_val != 0):
+		sharpe = map(lambda x : (x/std) * math.sqrt(365.35), returns)
+	
+	dict = {}
+	dict["Mean"] = mean
+	dict["Std"] = std
+	dict["Max Drawdown"] = max_drawdown
+	dict["Sharpe Ratio"] = sharpe
+	
+	with open('/Users/akshaankakar/Desktop/Big_Data_Analytics/Final/json/metrics/' + filename, 'w') as outfile:
+		json.dump(dict,outfile)
+
